31d7ad07ed22009ab80360471fe86b802ab62e92
Virjid <Virjid@163.com>
2020-05-11 01:08:09 +0800

    添加了控制是否序列化的手段
    可以使用Java自带的关键字transient，或者注解Transient来控制某个字段是否需要进行JSON序列化

--- src/main/java/me/virjid/karting/json/annotation/Transient.java
+++ src/main/java/me/virjid/karting/json/annotation/Transient.java
@@ -1,7 +1,12 @@
 package me.virjid.karting.json.annotation;
 
+import java.lang.annotation.*;
+
 /**
  * @author Virjid
  */
+@Documented
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Transient {
 }
