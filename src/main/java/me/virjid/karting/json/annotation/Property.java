220410e12e2f5b169ef94e948fdb07e81c141d8c
Virjid <Virjid@163.com>
2020-05-13 10:57:53 +0800

    添加了「对JSON序列化时，自定义字段映射」的支持 :+1:

---
+++ src/main/java/me/virjid/karting/json/annotation/Property.java
@@ -0,0 +1,13 @@
+package me.virjid.karting.json.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * @author Virjid
+ */
+@Documented
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Property {
+    String value() default "";
+}
